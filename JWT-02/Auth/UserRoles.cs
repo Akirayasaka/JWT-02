1bb9cf5b52de1c0a6c3a87818f7b43af5e853162
Akira <kuramal12@msn.com>
2022-02-03 01:10:19 +0800

    加入專案檔案。

---
+++ JWT-02/Auth/UserRoles.cs
@@ -0,0 +1,11 @@
+﻿namespace JWT_02.Auth
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Chairman = "Chairman";
+        public const string Supervisor = "Supervisor";
+        public const string Employee = "Employee";
+        public const string User = "User";
+    }
+}
